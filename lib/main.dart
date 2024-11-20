bf57acef6e6808433b51334a73cf1966217a1a1f
ayaj1021 <120981954+ayaj1021@users.noreply.github.com>
2024-11-20 06:28:32 +0100

    worked on app push notification

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,24 @@
 import 'package:abakon/core/navigation/router.dart';
 import 'package:abakon/core/theme/app_theme.dart';
+import 'package:abakon/firebase_options.dart';
 import 'package:abakon/presentation/general_widgets/app_overlay.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:onesignal_flutter/onesignal_flutter.dart';
 
-void main() {
+Future<void> main() async { 
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  OneSignal.Debug.setLogLevel(OSLogLevel.verbose);
+  OneSignal.initialize("6f4849a8-6488-4e71-a7fa-66e663f984c2");
+  OneSignal.Notifications.requestPermission(true);
+//6f4849a8-6488-4e71-a7fa-66e663f984c2
   runApp(const ProviderScope(child: MyApp()));
 }
 
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,14 @@
 import FlutterMacOS
 import Foundation
 
+import firebase_core
 import flutter_secure_storage_macos
 import local_auth_darwin
 import path_provider_foundation
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   FLALocalAuthPlugin.register(with: registry.registrar(forPlugin: "FLALocalAuthPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
